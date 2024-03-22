60a7011549d52c14e28db2ba6c78b94392fbebaa
Sarthak Joshi <141836239+sarthakdjoshi@users.noreply.github.com>
2024-03-22 18:01:02 +0530

    Commit

--- lib/Screen/checkout.dart
+++ lib/Screen/checkout.dart
@@ -1,31 +1,32 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:users/Model/Product_Model.dart';
-
 import '../Model/Cart_Model.dart';
 import 'Prodcut_detail.dart';
 
 class Checkout extends StatefulWidget {
-  final String ? productid;
-  const Checkout({Key? key, this.productid}) : super(key: key);
+  final String? productid;
+
+  const Checkout({super.key, this.productid});
 
   @override
   State<Checkout> createState() => _CheckoutState();
 }
 
 class _CheckoutState extends State<Checkout> {
-  var abc= FirebaseFirestore.instance.collection("Cart").snapshots();
+  var _abcStream = FirebaseFirestore.instance.collection("Cart").snapshots();
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    if(widget.productid!=null){
-      abc=FirebaseFirestore.instance.collection("Cart").where("pid",isEqualTo: widget.productid).snapshots();
+    if (widget.productid != null) {
+      _abcStream = FirebaseFirestore.instance
+          .collection("Cart")
+          .where("pid", isEqualTo: widget.productid)
+          .snapshots();
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,7 +36,7 @@ class _CheckoutState extends State<Checkout> {
         backgroundColor: Colors.indigo,
       ),
       body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-        stream:abc,
+        stream: _abcStream,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
@@ -48,176 +49,165 @@ class _CheckoutState extends State<Checkout> {
                 .toList();
 
             double totalNewPrice = 0;
-            carts.forEach((cart) {
+            for (var cart in carts) {
               totalNewPrice +=
                   double.parse(cart.qty) * double.parse(cart.price_new);
-            });
+            }
             double totalOldPrice = 0;
-            carts.forEach((cart) {
+            for (var cart in carts) {
               totalOldPrice +=
                   double.parse(cart.qty) * double.parse(cart.price_old);
-            });
-            double total = 0;
-            total=totalOldPrice-totalNewPrice;
+            }
+            double total = totalOldPrice - totalNewPrice;
 
             return Column(
               children: [
                 Expanded(
-                  child: Column(
-                    children: [
-                      Expanded(
-                        child: ListView.builder(
-                          itemCount: carts.length,
-                          itemBuilder: (context, index) {
-                            var cart = carts[index];
-                            return (cart.uid ==
-                                    FirebaseAuth.instance.currentUser?.uid)
-                                ? Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Card(
-                                      margin: const EdgeInsets.all(8),
-                                      elevation: 4,
-                                      child: InkWell(
-                                        onTap: () {
-                                          Navigator.push(
-                                            context,
-                                            MaterialPageRoute(
-                                              builder: (context) => ProductDetail(
-                                                productname: cart.product_name,
-                                              ),
-                                            ),
-                                          );
-                                        },
-                                        child: ListTile(
-                                          leading: Image.network(
-                                            cart.images[0],
-                                            fit: BoxFit.cover,
-                                            width: 80,
-                                          ),
-                                          title: Text(
-                                            cart.product_name,
-                                            style: const TextStyle(
-                                              fontSize: 16,
-                                              fontWeight: FontWeight.bold,
-                                            ),
-                                          ),
-                                          subtitle: Column(
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.start,
-                                            children: [
-                                              const SizedBox(height: 8),
-                                              Column(
-                                                children: [
-                                                  Text(
-                                                    "MRP: ₹${cart.price_old}",
-                                                    style: const TextStyle(
-                                                      fontSize: 14,
-                                                      color: Colors.red,
-                                                      decoration: TextDecoration
-                                                          .lineThrough,
-                                                      decorationColor: Colors.red,
-                                                    ),
-                                                  ),
-                                                  const SizedBox(height: 8),
-                                                  Text(
-                                                    "Qty: ${cart.qty.toString()}",
-                                                    style: const TextStyle(
-                                                        fontSize: 14),
-                                                  ),
-                                                  const SizedBox(
-                                                    height: 8,
-                                                  ),
-                                                  Text(
-                                                    "Price: ₹${cart.price_new}",
-                                                    style: const TextStyle(
-                                                      fontSize: 14,
-                                                      color: Colors.green,
-                                                    ),
-                                                  ),
-                                                ],
-                                              ),
-                                            ],
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                )
-                                : const SizedBox.shrink();
-                          },
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 8, vertical: 8),
+                  child: ListView.builder(
+                    itemCount: carts.length,
+                    itemBuilder: (context, index) {
+                      var cart = carts[index];
+                      return (cart.uid ==
+                          FirebaseAuth.instance.currentUser?.uid)
+                          ? Padding(
+                        padding: const EdgeInsets.all(8.0),
                         child: Card(
-                          child: Column(
-                            children: [
-                              const Row(
+                          margin: const EdgeInsets.all(8),
+                          elevation: 4,
+                          child: InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => ProductDetail(
+                                    productname: cart.product_name,
+                                  ),
+                                ),
+                              );
+                            },
+                            child: ListTile(
+                              leading: Image.network(
+                                cart.images[0],
+                                fit: BoxFit.cover,
+                                width: 80,
+                              ),
+                              title: Text(
+                                cart.product_name,
+                                style: const TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              subtitle: Column(
+                                crossAxisAlignment:
+                                CrossAxisAlignment.start,
                                 children: [
+                                  const SizedBox(height: 8),
                                   Text(
-                                    "Price Detail",
-                                    style: TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.w900),
+                                    "MRP: ₹${cart.price_old}",
+                                    style: const TextStyle(
+                                      fontSize: 14,
+                                      color: Colors.red,
+                                      decoration:
+                                      TextDecoration.lineThrough,
+                                      decorationColor: Colors.red,
+                                    ),
                                   ),
-                                ],
-                              ),
-                              Row(
-                                children: [
-                                  const Column(
-                                    children: [
-                                      Text(
-                                        "Product Price:",
-                                        style: TextStyle(fontSize: 20),
-                                      ),
-                                      Text(
-                                        "Total Discount:",
-                                        style: TextStyle(fontSize: 20),
-                                      ),
-                                      Text(
-                                        "Delivery Charge:",
-                                        style: TextStyle(fontSize: 20),
-                                      ),
-                                    ],
+                                  const SizedBox(height: 8),
+                                  Text(
+                                    "Qty: ${cart.qty.toString()}",
+                                    style: const TextStyle(fontSize: 14),
                                   ),
                                   const SizedBox(
-                                    width: 150,
+                                    height: 8,
                                   ),
-                                  Column(
-                                    children: [
-                                      Text(
-                                        totalNewPrice.toString(),
-                                        style: const TextStyle(fontSize: 20),
-                                      ),
-                                       Text(
-                                        total.toString(),
-                                        style: TextStyle(fontSize: 20),
-                                      ),
-                                       Text(
-                                        (totalNewPrice<10000)?"50.00":"0.00",
-                                        style: TextStyle(fontSize: 20),
-                                      ),
-                                    ],
+                                  Text(
+                                    "Price: ₹${cart.price_new}",
+                                    style: const TextStyle(
+                                      fontSize: 14,
+                                      color: Colors.green,
+                                    ),
                                   ),
                                 ],
                               ),
-                            const  Divider(
-                                height: 20,
-                              ),
-                              Row(
-                                children: [
-                                  const Text("Subtotal:"),
-                                  const SizedBox(
-                                    width: 180,
-                                  ),
-                                  Text(totalNewPrice.toString()),
-                                ],
-                              )
-                            ],
+                            ),
                           ),
                         ),
                       )
-                    ],
+                          : const SizedBox.shrink();
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    child: Column(
+                      children: [
+                        const Row(
+                          children: [
+                            Expanded(
+                              child: Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Text(
+                                  "Price Detail",
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w900,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  buildPriceDetailRow(
+                                    "Product Price:",
+                                    totalNewPrice.toString(),
+                                  ),
+                                  buildPriceDetailRow(
+                                    "Total Discount:",
+                                    total.toString(),
+                                  ),
+                                  buildPriceDetailRow(
+                                    "Delivery Charge:",
+                                    (totalNewPrice < 10000) ? "50.00" : "0.00",
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        const Divider(
+                          height: 20,
+                        ),
+                        Row(
+                          children: [
+                            const Expanded(
+                              child: Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Text(
+                                  "Subtotal:",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Text(
+                              totalNewPrice.toString(),
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
                 Padding(
@@ -227,18 +217,30 @@ class _CheckoutState extends State<Checkout> {
                     children: [
                       Text(
                         "Total: ₹${totalNewPrice.toStringAsFixed(2)}",
-                        // Display total price
                         style: const TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold),
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                       Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 10, vertical: 10),
                         child: SizedBox(
                           width: double.infinity,
                           child: ElevatedButton(
                             onPressed: () {},
-                            style: ElevatedButton.styleFrom(shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero)),
-                            child: const Text("Place Order",style: TextStyle(color: Colors.indigo,fontSize:15),),
+                            style: ElevatedButton.styleFrom(
+                              shape: const RoundedRectangleBorder(
+                                borderRadius: BorderRadius.zero,
+                              ),
+                            ),
+                            child: const Text(
+                              "Place Order",
+                              style: TextStyle(
+                                color: Colors.indigo,
+                                fontSize: 15,
+                              ),
+                            ),
                           ),
                         ),
                       ),
@@ -249,6 +251,29 @@ class _CheckoutState extends State<Checkout> {
             );
           }
         },
+      ),
+    );
+  }
+
+  Widget buildPriceDetailRow(String title, String amount) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 16,
+            ),
+          ),
+          Text(
+            "₹$amount",
+            style: const TextStyle(
+              fontSize: 16,
+            ),
+          ),
+        ],
       ),
     );
   }
